98ecc2b6e4143a2ce33c2939b9dba004f212e7a0
Junyu Liu <93633960+BGG-JunyuLiu@users.noreply.github.com>
2024-07-23 14:07:04 +1000

    Update

---
+++ PSF_comparison/generate_PSF_real.py
@@ -0,0 +1,108 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tifffile
+from scipy.special import j1
+from scipy.ndimage import shift
+from scipy.signal import convolve2d
+from scipy.integrate import simps
+
+
+def read_tiff(filename):
+    with tifffile.TiffFile(filename) as tf:
+        tif = np.ascontiguousarray(np.moveaxis(tf.asarray(), [0, 1, 2], [2, 1, 0]))
+
+    return tif
+
+
+def save_tiff(filename, data):
+    with tifffile.TiffWriter(filename, imagej=True) as tif:
+        if data.ndim == 4:
+            dat = np.moveaxis(data, [2, 3, 1, 0], [0, 1, 2, 3])
+            tif.save(dat[None, :, :, :, :].astype(np.float32))  # TZCYXS
+        elif data.ndim == 3:
+            d = np.moveaxis(data, [2, 1, 0], [0, 1, 2])
+            tif.save(d[None, :, None, :, :].astype(np.float32))  # TZCYXS
+
+
+def Gaussian_PSF(X, Y, Z, fwhm_x, fwhm_y, fwhm_z):
+    sigma_x = fwhm_x / (2 * np.sqrt(2 * np.log(2)))
+    sigma_y = fwhm_y / (2 * np.sqrt(2 * np.log(2)))
+    sigma_z = fwhm_z / (2 * np.sqrt(2 * np.log(2)))
+
+    psf = np.exp(-((X ** 2) / (2 * sigma_x ** 2) +
+                   (Y ** 2) / (2 * sigma_y ** 2) +
+                   (Z ** 2) / (2 * sigma_z ** 2)))
+
+    return psf
+
+
+if __name__ == '__main__':
+    NA = 0.8
+    lam = 561
+    n = 1.5
+
+    # generate grid
+    pixel_size = 20
+    size = 512
+
+    axis_seq = np.arange(-size // 2, size - size // 2) * pixel_size
+
+    X, Y, Z = np.meshgrid(axis_seq, axis_seq, axis_seq, indexing='ij')
+
+    # generate confocal PSF
+    fwhm_lateral = 0.51 * lam / NA
+    fwhm_axial = 1.4 * lam * n / (NA ** 2)
+
+    psf_confocal = Gaussian_PSF(X, Y, Z, fwhm_lateral, fwhm_lateral, fwhm_axial)
+
+    # generate widefield PSF
+    fwhm_lateral = 0.51 * lam / NA
+    fwhm_axial = 1.4 * lam * n / (NA ** 2) * np.sqrt(2)
+
+    psf_widefield = Gaussian_PSF(X, Y, Z, fwhm_lateral, fwhm_lateral, fwhm_axial)
+
+    save_tiff('./PSF/widefield.tif', psf_widefield)
+    save_tiff('./PSF/confocal.tif', psf_confocal)
+
+    # generate RO-iSCAT PSF
+    # rat = 0.40283650647
+    # psf_ro = np.zeros_like(psf_wf)
+    # for z in range(0, size):
+    #     for phi in range(0, 360, 10):
+    #         shift_x = np.abs(z - size // 2) * rat * pixel_size * np.sin(phi / 180 * np.pi)
+    #         shift_y = np.abs(z - size // 2) * rat * pixel_size * np.cos(phi / 180 * np.pi)
+    #         psf_ro[..., z] = psf_ro[..., z] + shift(psf_wf[..., z], shift=(shift_y, shift_x))
+
+    # generate RO PSF
+    rat = 0.40283650647
+    mask = np.zeros_like(psf_widefield)
+    for z in range(0, size):
+        R = np.sqrt(X[..., z] ** 2 + Y[..., z] ** 2)
+        Z = np.abs(z - size // 2)
+        mask[..., z][(R <= rat * (Z + 3) * pixel_size) & (R >= rat * Z * pixel_size)] = 1
+        mask[..., z] = mask[..., z] / (mask[..., z].sum() + 1e-7)
+
+    save_tiff('./PSF/mask.tif', mask)
+
+    otf_mask = np.fft.fftn(mask, axes=(0, 1))
+    otf_wf = np.fft.fftn(psf_widefield, axes=(0, 1))
+    otf_ro = otf_mask * otf_wf
+    psf_ro = np.real(np.fft.fftshift(np.fft.ifftn(otf_ro, axes=(0, 1)), axes=(0, 1)))
+
+    save_tiff('./PSF/rotational-oblique.tif', psf_ro)
+
+    # check PSF profiles
+    plt.figure()
+    psf_widefield = psf_widefield / psf_widefield.max()
+    psf_confocal = psf_confocal / psf_confocal.max()
+    psf_ro = psf_ro / psf_ro.max()
+
+    plt.plot(axis_seq, psf_widefield[size // 2, size // 2, :], 'g')
+    plt.plot(axis_seq, psf_confocal[size // 2, size // 2, :], 'k')
+    plt.plot(axis_seq, psf_ro[size // 2, size // 2, :], 'r')
+    plt.show()
+
+    # plt.plot(axis_seq, psf_wf.sum(axis=(0, 1)), 'g')
+    # plt.plot(axis_seq, psf_confocal.sum(axis=(0, 1)), 'k')
+    # plt.plot(axis_seq, psf_ro.sum(axis=(0, 1)), 'r')
+    # plt.show()
